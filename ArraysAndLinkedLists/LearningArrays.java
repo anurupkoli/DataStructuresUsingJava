e3c3bfd73e48dd268e71698aa356eec071b652d8
anurupkoli <anurupkoli01@gmail.com>
2023-12-17 16:45:44 +0530

    Learning DataStructures in Java

---
+++ ArraysAndLinkedLists/LearningArrays.java
@@ -0,0 +1,21 @@
+package ArraysAndLinkedLists;
+
+import java.util.Scanner;
+
+public class LearningArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        sc.close();
+    }
+}
