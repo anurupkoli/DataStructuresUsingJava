d425f99b7551960cffe7114c93a780465e61d44a
anurupkoli <anurupkoli01@gmail.com>
2023-12-23 21:35:53 +0530

    Binary search problem

---
+++ BinarySearch/MountainArray.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class MountainArray {
+    static int binarySearchMountain(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<end) {
+            int mid = start + (end - start)/2;
+            // if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+            //     return arr[mid];
+            // }
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return arr[start];
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,8};
+        System.out.println(binarySearchMountain(arr));
+    }
+}
