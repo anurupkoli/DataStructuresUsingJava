48f535ca6a75eb74c6d1b54889984a6ad77dd4f3
anurupkoli <anurupkoli01@gmail.com>
2023-12-31 15:19:28 +0530

    added better code

--- ArraysAndLinkedLists/Arrays/TrappingRainWater.java
+++ ArraysAndLinkedLists/Arrays/TrappingRainWater.java
@@ -1,29 +1,39 @@
 package ArraysAndLinkedLists.Arrays;
 
+import java.util.Arrays;
+
 public class TrappingRainWater {
     public static int trappingRainWater(int[] arr) {
         int[] maxLefVal = new int[arr.length];
+        maxLefVal[0] = arr[0];
         int[] maxRightVal = new int[arr.length];
+        maxRightVal[arr.length-1] = arr[arr.length-1];
         if(arr.length <= 2){
             return 0;
         }
-        for (int i = 0; i < arr.length; i++) {
-            int rightMax = Integer.MIN_VALUE;
-            int leftMax = Integer.MIN_VALUE;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] > rightMax) {
-                    rightMax = arr[j];
-                }
-            }
-            for (int j = i; j >= 0; j--) {
-                if (arr[j] > leftMax) {
-                    leftMax = arr[j];
-                }
-            }
-            maxRightVal[i] = rightMax;
-            maxLefVal[i] = leftMax;
+        // for (int i = 0; i < arr.length; i++) {
+        //     int rightMax = Integer.MIN_VALUE;
+        //     int leftMax = Integer.MIN_VALUE;
+        //     for (int j = i; j < arr.length; j++) {
+        //         if (arr[j] > rightMax) {
+        //             rightMax = arr[j];
+        //         }
+        //     }
+        //     for (int j = i; j >= 0; j--) {
+        //         if (arr[j] > leftMax) {
+        //             leftMax = arr[j];
+        //         }
+        //     }
+        //     maxRightVal[i] = rightMax;
+        //     maxLefVal[i] = leftMax;
+        // }
+        for (int i = 1; i < arr.length; i++) {
+            maxLefVal[i] = Math.max(arr[i], maxLefVal[i-1]);
         }
-
+        for (int i = arr.length-2; i >= 0; i--) {
+            maxRightVal[i] = Math.max(arr[i], maxRightVal[i+1]);
+        }
+        
         int waterVolume = 0;
         for (int i = 0; i < arr.length; i++) {
             int min = Math.min(maxLefVal[i], maxRightVal[i]);
@@ -38,7 +48,7 @@ public class TrappingRainWater {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6 };
+        int[] arr = {4,2,0,6,3,2,5};
         System.out.println(trappingRainWater(arr));;
     }
 }
