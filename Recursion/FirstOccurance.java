dfc50e28d76f8613ca6ca1c19695867828e1b3c4
anurupkoli <anurupkoli01@gmail.com>
2024-01-16 14:24:11 +0530

    lastOccurance

--- Recursion/FirstOccurance.java
+++ Recursion/FirstOccurance.java
@@ -10,8 +10,19 @@ public class FirstOccurance {
         }
         return firstOccurance(arr, ele, index+1);
     }
+
+    static int lastOccurance(int[] arr, int ele, int index){
+        if(index < 0){
+            return -1;
+        }
+        if(arr[index] == ele){
+            return index;
+        }
+        return lastOccurance(arr, ele, index-1);
+    }
     public static void main(String[] args) {
         int[] arr = {1,3,51,1,3,7,89,4};
-        System.out.println(firstOccurance(arr, 51, 0));
+        System.out.println(firstOccurance(arr, 1, 0));
+        System.out.println(lastOccurance(arr, 1, arr.length-1));
     }
 }
