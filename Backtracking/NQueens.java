752ac55590a1f9aad9d78633e51bb30a84dd83d6
anurupkoli <anurupkoli01@gmail.com>
2024-01-28 14:40:05 +0530

    Modified NQueens

--- Backtracking/NQueens.java
+++ Backtracking/NQueens.java
@@ -8,13 +8,40 @@ public class NQueens {
             return;
         }
         for (int j = 0; j < board.length; j++) {
-            board[i][j] = 'Q';
-            nQueens(board, i+1);
-            board[i][j] = '_';
+            if (isSafe(board, i, j)) {
+                board[i][j] = 'Q';
+                nQueens(board, i + 1);
+                board[i][j] = '_';
+            }
         }
     }
 
-    public static void printBoard(char[][] board){
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
@@ -22,8 +49,9 @@ public class NQueens {
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        char[][] board = new char[6][6];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 board[i][j] = '_';
