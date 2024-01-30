1b952ca72964b57378dce81a8be3e50559b30225
anurupkoli <anurupkoli01@gmail.com>
2024-01-30 18:14:41 +0530

    optimized container with most water code

--- ArraysAndLinkedLists/ArrayList/ContainerWithMostWater.java
+++ ArraysAndLinkedLists/ArrayList/ContainerWithMostWater.java
@@ -6,11 +6,25 @@ public class ContainerWithMostWater {
     public static int containerWithMostWater(ArrayList<Integer> arr) {
         int waterVolume = 0;
 
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = i + 1; j < arr.size(); j++) {
-                int height = Math.min(arr.get(i), arr.get(j));
-                int width = j - i;
-                waterVolume = Math.max(waterVolume, height*width);
+        // for (int i = 0; i < arr.size(); i++) {
+        //     for (int j = i + 1; j < arr.size(); j++) {
+        //         int height = Math.min(arr.get(i), arr.get(j));
+        //         int width = j - i;
+        //         waterVolume = Math.max(waterVolume, height*width);
+        //     }
+        // }
+
+        int leftP = 0;
+        int rightP = arr.size() - 1;
+        while (leftP < rightP) {
+            int height = Math.min(arr.get(leftP), arr.get(rightP));
+            int width = rightP - leftP;
+            waterVolume = Math.max(waterVolume, height * width);
+            if(leftP <= rightP){
+                leftP++;
+            }
+            else{
+                rightP--;
             }
         }
 
