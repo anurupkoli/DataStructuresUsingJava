446a111fd22495094ab559bae86bf718c2f9a609
anurupkoli <anurupkoli01@gmail.com>
2024-02-03 14:19:46 +0530

    more linked list methods

--- ArraysAndLinkedLists/LinkedLists/LinkedList.java
+++ ArraysAndLinkedLists/LinkedLists/LinkedList.java
@@ -11,12 +11,14 @@ class LinkedListL{
     }
 
     Node head, tail;
+    public int length = 0;
     LinkedListL(){
         this.head = null;
         this.tail = null;
     }
 
     public void addFirst(int data){
+        length++;
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
@@ -25,8 +27,9 @@ class LinkedListL{
         newNode.next = head;
         head = newNode;
     }
-
-    public void addLast(int data){
+   
+    public void add(int data){
+        length++;
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
@@ -36,7 +39,31 @@ class LinkedListL{
         tail = newNode;
     }
 
+    public void add(int index, int data){
+        length++;
+        Node newNode = new Node(data);
+        Node temp = head;
+        if(index >= this.length || index < 0){
+            System.out.println("Index out of bound");
+            return;
+        }
+        if(index == 0){
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        for (int i = 0; i < index-1; i++) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     public void print(){
+        if(head == null){
+            System.out.println("Empty!");
+            return;
+        }
         Node travNode = head;
         while(travNode != null){
             System.out.print(travNode.data + " ");
@@ -48,9 +75,10 @@ class LinkedListL{
 public class LinkedList {
     public static void main(String[] args) {
         LinkedListL ll = new LinkedListL();
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(3);
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(0, 5);
         ll.print();
     }
 }
