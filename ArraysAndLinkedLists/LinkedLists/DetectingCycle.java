450e1bc132ad398d9f82c4a41e6c03d5161e7198
anurupkoli <anurupkoli01@gmail.com>
2024-02-04 14:46:57 +0530

    removing cycle algorithm

--- ArraysAndLinkedLists/LinkedLists/DetectingCycle.java
+++ ArraysAndLinkedLists/LinkedLists/DetectingCycle.java
@@ -1,5 +1,4 @@
 package ArraysAndLinkedLists.LinkedLists;
-import ArraysAndLinkedLists.LinkedLists.LinkedListL;
 
 class NewLinkedList extends LinkedListL{
     public boolean detectCycle(){
@@ -16,6 +15,36 @@ class NewLinkedList extends LinkedListL{
         }
         return false;
     }
+
+    //removing cycle
+    public void removeCycle(){
+        Node slowNode = head;
+        Node fastNode = head;
+        boolean isCycle = false;
+        
+        while (fastNode != null && fastNode.next != null) {
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+            if(fastNode == slowNode){
+                isCycle = true;
+                break;
+            }
+        }
+
+        if(!isCycle){
+            System.out.println("Is not a cycle");
+            return;
+        }
+
+        Node prevNode = null;
+        while (fastNode != slowNode) {
+            prevNode = fastNode;
+            fastNode = fastNode.next;
+            slowNode = slowNode.next;
+        }
+
+        prevNode.next = null;
+    }
 }
 
 public class DetectingCycle {
@@ -25,7 +54,7 @@ public class DetectingCycle {
         ll.add(2);
         ll.add(3);
         ll.add(4);
-        System.out.println(ll.detectCycle());
+        ll.removeCycle();
         ll.print();
     }
 }
