627e456a33a6b0b18ee8e1f592c9d381c6304e71
anurupkoli <anurupkoli01@gmail.com>
2024-02-07 17:24:10 +0530

    Stacks using array lists

---
+++ Stacks/StackUsingArrayList.java
@@ -0,0 +1,37 @@
+package Stacks;
+
+import java.util.ArrayList;
+
+class Stack{
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public boolean isEmpty(){
+        return list.size() == 0;
+    }
+
+    public void push(int data){
+        list.add(data);
+    }
+
+    public int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int data = list.get(list.size() - 1);
+        list.remove(list.size() - 1 );
+        return data;
+    }
+
+    public int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return list.get(list.size() - 1);
+    }
+}
+
+public class StackUsingArrayList {
+    public static void main(String[] args) {
+        
+    }
+}
