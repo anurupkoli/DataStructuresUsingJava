19a4f5ae7cd40d458c19715df018ee91d1fbeea4
anurupkoli <anurupkoli01@gmail.com>
2024-02-14 18:40:12 +0530

    More functions for binary serach tree

--- BinaryTrees/BinarySearchTrees.java
+++ BinaryTrees/BinarySearchTrees.java
@@ -1,16 +1,16 @@
 package BinaryTrees;
 
-class BinarySearchTree{
+class BinarySearchTree {
     /**
-     *  Node
+     * Node
      */
     public class Node {
-    
+
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
@@ -18,31 +18,32 @@ class BinarySearchTree{
     }
 
     Node root;
-    BinarySearchTree(){
+
+    BinarySearchTree() {
         this.root = null;
     }
 
-    private Node insertNodeRec(Node root, int data){
+    private Node insertNodeRec(Node root, int data) {
         Node newNode = new Node(data);
-        if(root == null){
+        if (root == null) {
             root = newNode;
             return root;
         }
-        if(data < root.data){
+        if (data < root.data) {
             root.left = insertNodeRec(root.left, data);
         }
-        if(data > root.data){
+        if (data > root.data) {
             root.right = insertNodeRec(root.right, data);
         }
         return root;
     }
 
-    public void insertNode(int data){
+    public void insertNode(int data) {
         root = insertNodeRec(this.root, data);
     }
 
-    private void inOrderRec(Node root){
-        if(root == null){
+    private void inOrderRec(Node root) {
+        if (root == null) {
             return;
         }
         inOrderRec(root.left);
@@ -50,19 +51,78 @@ class BinarySearchTree{
         inOrderRec(root.right);
     }
 
-    public void inOrder(){
+    public void inOrder() {
         inOrderRec(root);
         System.out.println();
+    }
+
+    private boolean searchRec(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (key < root.data) {
+            return searchRec(root.left, key);
+        } else if (key > root.data) {
+            return searchRec(root.right, key);
+        } else if (key == root.data) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int data) {
+        if(root == null){
+            return null;
+        }
+        if (data < root.data) {
+            root.left = deleteRec(root.left, data);
+        }
+        if (data > root.data) {
+            root.right = deleteRec(root.right, data);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            Node InorSucc = findInorderSuccessor(root.right);
+            root.data = InorSucc.data;
+            root.right = deleteRec(root.right, InorSucc.data);
+        }
+
+        return root;
+    }
+
+    private Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public void delete(int key) {
+       root = deleteRec(root, key);
     }
 }
 
 public class BinarySearchTrees {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        int[] arr = {5,1,3,4,2,7};
+        int[] arr = { 5, 1, 3, 4, 2, 7, 9, 10 };
         for (int i = 0; i < arr.length; i++) {
             tree.insertNode(arr[i]);
         }
+        tree.inOrder();
+        tree.delete(10);
         tree.inOrder();
     }
 }
