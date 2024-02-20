ed93ad1ba1d281313fc9e6c232cb8568e0a7337c
anurupkoli <anurupkoli01@gmail.com>
2024-02-20 21:12:06 +0530

    Learning about PriorityQueue with comparable interface

--- Heaps/PriorityQueues.java
+++ Heaps/PriorityQueues.java
@@ -1,13 +1,33 @@
 package Heaps;
 import java.util.*;
 
+class Student implements Comparable<Student>{
+    int rank;
+    String name;
+
+    Student(String name, int rank){
+        this.name = name;
+        this.rank = rank;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        // TODO Auto-generated method stub
+        return this.rank - o.rank;
+    }
+
+}
+
 public class PriorityQueues{
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(4);
-        pq.add(1);
-        pq.add(5);
-        pq.add(3);
-        System.out.println(pq.toString());
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 12));
+        pq.add(new Student("B", 1));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 13));
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
+            pq.remove();
+        }
     }
 }
