19f5b980e4eaeda20ca5893c12f09c0f14c1c17d
anurupkoli <anurupkoli01@gmail.com>
2024-02-25 17:47:35 +0530

    updated

--- Heaps/CostOfConnectingNRopes.java
+++ Heaps/CostOfConnectingNRopes.java
@@ -4,13 +4,19 @@ import java.util.PriorityQueue;
 
 public class CostOfConnectingNRopes {
     public static void main(String[] args) {
-        int[] ropes = {2,3,3,4,6};
+        int[] ropes = {6, 4, 3, 3, 2};
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < ropes.length; i++) {
             pq.add(ropes[i]);
         }
-
+        for (int i = 0; i < ropes.length; i++) {
+            System.out.print(" " + pq.remove());
+        }
+        System.out.println();
+        for (int i = 0; i < ropes.length; i++) {
+            pq.add(ropes[i]);
+        }
         int cost = 0;
         while (pq.size() > 1) {
             int min1 = pq.remove();
