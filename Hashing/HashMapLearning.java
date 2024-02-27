5e839822be34679cbd4809554d0e553df74d601a
anurupkoli <anurupkoli01@gmail.com>
2024-02-27 17:52:37 +0530

    Learning HashMap

---
+++ Hashing/HashMapLearning.java
@@ -0,0 +1,25 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class HashMapLearning{
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("India", 150);
+        map.put("Australia", 100);
+        map.put("England", 80);
+
+        // System.out.println(map.get("India"));
+        // System.out.println(map.get("USA"));
+        // System.out.println(map);
+
+        // System.out.println(map.containsKey("India"));
+        // System.out.println(map.remove("Australia"));
+        // System.out.println(map);
+
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        map.clear();
+        System.out.println(map);
+    }
+}
