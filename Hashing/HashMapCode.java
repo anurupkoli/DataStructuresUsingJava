5341903bfcf4fdf94184242c9a85ae67f9f8e53c
anurupkoli <anurupkoli01@gmail.com>
2024-02-28 20:47:08 +0530

    adding more functions to hashmap

--- Hashing/HashMapCode.java
+++ Hashing/HashMapCode.java
@@ -38,6 +38,22 @@ class HashMapNew<K, V> {
         }
         return -1;
     }
+    @SuppressWarnings("unchecked")
+    private void reHash(){
+        LinkedList<Node> oldBucket[] = buckets;
+        buckets = new LinkedList[size*2];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+
+        for (int i = 0; i < oldBucket.length; i++) {
+            LinkedList<Node> ll = oldBucket[i];
+            for (int j = 0; j < ll.size(); j++) {
+                Node node = ll.remove();
+                put(node.key, node.value);
+            }
+        }
+    }
     public void put(K key, V value){
         int bi = hashFunction(key);
         int ki = searchInLL(key, bi);
